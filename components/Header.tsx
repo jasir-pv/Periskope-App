8f1493298d5123f0859d6564bfe2bc18850f13c0
jasir-pv <jasirahsanpv40@gmail.com>
2025-05-24 10:25:00 +0530

    feat: header section completed

---
+++ components/Header.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import React from 'react'
+import { FaQuestionCircle, FaStar, FaSyncAlt } from 'react-icons/fa'
+import { MdNotificationsOff, MdOutlineSort } from 'react-icons/md'
+import { RiChatSmileAiFill, RiExpandUpDownLine } from 'react-icons/ri'
+import { VscDesktopDownload } from 'react-icons/vsc'
+import { WiStars } from 'react-icons/wi'
+
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <div className='p-2 bg-primary-light border-b border-gray-300 flex justify-between items-center'>
+        <div className='flex flex-row  items-center gap-2 ml-4'>
+                <RiChatSmileAiFill  className='text-muted'/>           
+                <h2 className='text-xs text-muted font-sans font-semibold'>Chats</h2>
+        </div>
+
+          <div className='flex gap-4'>
+              <div className='flex items-center text-xs gap-1 text-muted p-1 border'>
+                <FaSyncAlt /> 
+              <button> Refresh</button>
+              </div>
+
+             <div className='flex items-center text-xs gap-1 text-muted p-1 border'>
+                <FaQuestionCircle /> 
+              <button> Help</button>
+              </div>
+             <div className='flex items-center text-xs gap-1 text-muted p-1 border'>
+                <FaStar className='text-yellow-400'/> 
+              <button> 5/8 Phones</button>
+              <RiExpandUpDownLine />
+              </div>
+
+
+              <div className='flex items-center text-xs gap-1 text-muted p-1 border'>
+                 <VscDesktopDownload /> 
+              </div>
+
+              <div className='flex items-center text-xs gap-1 text-muted p-1 border'>
+                <MdNotificationsOff  /> 
+              </div>
+
+              <div className='flex items-center text-xs gap-1 text-muted p-1 border'>
+                <WiStars />
+                <MdOutlineSort /> 
+              </div>
+          </div>
+
+    </div>
+  )
+}
+
+export default Header
